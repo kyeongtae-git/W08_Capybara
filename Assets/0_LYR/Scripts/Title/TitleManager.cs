adf744afc00bae32778578a07a7fbb9525208e84
je-ya <psychicartil@gmail.com>
2025-04-23 16:42:31 +0900

    [Add] 타이틀 추가

---
+++ Assets/0_LYR/Scripts/Title/TitleManager.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleManager : MonoBehaviour
+{
+
+    public void OnStartButtonClick()
+    {
+        SceneManager.LoadScene("MainScene_1");
+    }
+
+
+    public void OnExitButtonClick()
+    {
+        Application.Quit();
+
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#endif
+    }
+}
