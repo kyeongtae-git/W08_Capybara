bc11c253b42dfa12f406f524a885d1233a8101b2
scopedogtc <rhdrks1440@gmail.com>
2025-04-23 21:47:39 +0900

    수치 테스트용 스크립트

---
+++ Assets/0_KMJ/Scripts/BalanceTestScript.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class BalanceTestScript : MonoBehaviour
+{
+    float totalDamage = 0;
+    float basicDamage = 1;
+    float damageScale = 1.04f;
+    int currentBuff = 0;
+    int attackNumber = 10;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        for(int i=0; i<attackNumber; i++)
+        {
+            totalDamage += basicDamage * Mathf.Pow(damageScale, currentBuff);
+            currentBuff++;
+        }
+        Debug.Log(totalDamage / (basicDamage * attackNumber));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
