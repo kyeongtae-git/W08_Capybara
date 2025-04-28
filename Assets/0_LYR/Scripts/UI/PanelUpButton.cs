e39e53e43e38f574fce846b40d544256404cae8f
kyeongtae-git <choikt9012@gmail.com>
2025-04-28 20:52:28 +0900

    Revert "[FIX]"
    This reverts commit bf0f950bb557af439c8ceb51d96941101f2af4fd.

--- Assets/0_LYR/Scripts/UI/PanelUpButton.cs
+++ Assets/0_LYR/Scripts/UI/PanelUpButton.cs
@@ -9,21 +9,22 @@ public class PanelUpButton : MonoBehaviour
     UI_SkillSeletctionUp _upButtonUI;
 
 
-    void OnEnable()
+    void Start()
     {
         _skillCanvas = FindAnyObjectByType<UI_SkillSelection>().gameObject.GetComponent<Canvas>();
         _downbuttonUI = FindAnyObjectByType<UI_SkillSeletctionDown>();
         _upButtonUI = FindAnyObjectByType<UI_SkillSeletctionUp>();
         _panelUpButton = GetComponent<Button>();
 
+        ButtonClick();
+    }
+
+    public void ButtonClick()
+    {
         _panelUpButton.onClick.RemoveAllListeners();
         _panelUpButton.onClick.AddListener(() => _skillCanvas.enabled = true);
         _panelUpButton.onClick.AddListener(() => _downbuttonUI.CanvasOnOff(true));
         _panelUpButton.onClick.AddListener(() => _upButtonUI.CanvasOnOff(false));
-    }
 
-    private void OnDisable()
-    {
-        _panelUpButton.onClick.RemoveAllListeners();
     }
 }
